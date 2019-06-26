8750ce7125b49288b3a591015a386ee387a6a5dc
alexander-morris <alex@theblockchaininstitute.org>
2019-06-26 16:08:11 -0500

    fixing typos and justifying

---
+++ docs/docs/docs/js/script.js
@@ -0,0 +1,138 @@
+angular.module('autonomous', [])
+.controller('headerCtrl',[ '$http', '$scope', '$window', '$interval', function( $http, $scope, $window, $interval ){
+	$scope.currentName = "what";
+	$scope.init = function () {
+		console.log('scope.init ran')
+		$scope.counter = 0;
+		$scope.currentName = "Progress"
+		$scope.headerText = "starts with people."
+
+	    $interval(function(){
+	        getWhat();
+	    },1500);	
+
+	}
+
+	function getWhat () {
+		var options = [
+			["Progress","starts with people."],
+			["Progress","start with people."],
+			["Projects","start with people."],
+			["Projects","start with people."],
+			["Projects","start with people."],
+			["Projects","start with people."],
+			["People","start with people."],
+			["People","start with people."],
+			["People","start with people."],
+			["People","start with people."]
+		]
+
+		$scope.currentName = (options[$scope.counter])[0];
+
+		$scope.counter++
+		setTimeout(function(){
+
+			$scope.headerText = (options[$scope.counter - 1])[1];
+			return
+		}, 100);
+	}
+	
+	$scope.click = function () {
+		console.log('click', $scope.currentName)
+	}
+
+
+
+	$scope.init()
+}])
+.controller('viewController',[ '$http', '$scope', '$window', '$interval', function( $http, $scope, $window, $interval ){
+
+	$scope.init = function () {
+		$scope.isHidden = {
+			"home" : "",
+			"members" : "hidden",
+			"research" : "hidden"
+		}
+
+
+		$scope.memberTypes = [
+			"software",
+			"operations",
+			"marketing",
+			"financial"
+		]
+
+		$scope.memberList = [{
+			"name" : "Alex Morris",
+			"type" : "software",
+			"title" : "Software Developer",
+			"bio" : "Alex is a seasoned software developer with experience in DLT as well as more traditional systems.",
+			"imageUrl" : "https://static.theblockchaininstitute.org/wp-content/uploads/2019/05/Web_ALex-768x768.jpg"
+
+		},{
+			"name" : "Jason Swearingen",
+			"type" : "marketing",
+			"title" : "Graphic Designer",
+			"bio" : "Jason is an experienced graphic designer. They currently manage the brand at theblockchaininstitute.org, and greatly enjoy communicating complex concepts visually.",
+			"imageUrl" : "https://static.theblockchaininstitute.org/wp-content/uploads/2019/05/Web_Jason-768x768.jpg"
+
+		},{
+			"name" : "Alex Morris",
+			"type" : "software",
+			"title" : "Software Developer",
+			"bio" : "Alex is a seasoned software developer with experience in DLT as well as more traditional systems.",
+			"imageUrl" : "https://static.theblockchaininstitute.org/wp-content/uploads/2019/05/Web_ALex-768x768.jpg"
+
+		},{
+			"name" : "Dan Cecelia",
+			"type" : "operations",
+			"title" : "Business Analyst",
+			"bio" : "Dan has experience growing businesses large and small.",
+			"imageUrl" : "https://static.theblockchaininstitute.org/wp-content/uploads/2019/05/Web_ALex-768x768.jpg"
+
+		}]
+
+		$scope.memberListFiltered = $scope.memberList;
+	}
+
+	$scope.navTo = function (div) {
+		if ( div === "members" ){
+			$scope.isHidden = {
+				"home" : "hidden",
+				"members" : "",
+				"research" : "hidden"
+			}			
+		} else if ( div === "research") {
+			$scope.isHidden = {
+				"home" : "hidden",
+				"members" : "hidden",
+				"research" : ""
+			}			
+		} else {
+			$scope.isHidden = {
+				"home" : "",
+				"members" : "hidden",
+				"research" : "hidden"
+			}			
+		}
+	}
+
+	$scope.filterMembers = function (type) {
+		var newList = [];
+		for ( var l = 0; l < $scope.memberList.length; l++ ) {
+			if ( $scope.memberList[l].type === type ) {
+				newList.push($scope.memberList[l])
+			}
+		}
+
+		if ( newList.length > 0 ) {
+			$scope.memberListFiltered = newList
+		} else {
+			$scope.memberListFiltered = $scope.memberList
+		}
+
+	}
+
+	$scope.init()
+
+}])
